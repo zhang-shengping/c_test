82c73a72873e5951b9aa3d377fbf1ea12c619c31
peter <zhangshengping2012@hotmail.com>
2019-04-11 00:11:32 -0700

    init

--- more01.c
+++ more01.c
@@ -3,5 +3,5 @@
 int main(int ac, char *av[]){
     printf("ac %d \n", ac);
     for(int i=0; i <= ac-1; ++i)
-        printf("*av[i] is %s \n ", av[i]);
+        printf("av[i] is %s \n ", av[i]);
 }
